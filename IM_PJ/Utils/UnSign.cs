bd4839048144d2cb99d9fa4702f64235e29a521f
Binh Tran <tranthaibinh111@yahoo.com>
2019-04-18 15:21:22 +0700

    The commint didn't enough file

---
+++ IM_PJ/Utils/UnSign.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace IM_PJ
+{
+    public class UnSign
+    {
+        public static string convert(string value)
+        {
+            //First to lower case
+            value = value.ToLower();
+
+            //Remove all accents
+            Regex regex = new Regex("\\p{IsCombiningDiacriticalMarks}+");
+            value = value.Normalize(System.Text.NormalizationForm.FormD);
+            value = regex.Replace(value, String.Empty).Replace('\u0111', 'd').Replace('\u0110', 'D');
+
+            return value;
+        }
+    }
+}
