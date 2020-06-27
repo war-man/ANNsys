34746d3ef837c78c273cc6bb4180223ba5531369
Binh Tran <tranthaibinh111@yahoo.com>
2020-06-27 16:47:58 +0700

    the stock transfer page is finish

---
+++ IM_PJ/Models/Pages/thong-ke-chuyen-kho/SubStockTransferReport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IM_PJ.Models.Pages.thong_ke_chuyen_kho
+{
+    public class SubStockTransferReport
+    {
+        public string sku { get; set; }
+        public string image { get; set; }
+        public int quantityTransfer { get; set; }
+        public int quantityAvailable { get; set; }
+        public DateTime transferDate { get; set; }
+    }
+}
